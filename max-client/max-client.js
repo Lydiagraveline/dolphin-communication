4774c773350b404bd102cd984399d1045233831e
Lydiagraveline <lydiagraveline20@gmail.com>
2023-11-22 10:49:21 -0500

    added the max-client folder

---
+++ max-client/max-client.js
@@ -0,0 +1,39 @@
+const { io } = require("socket.io-client"); // for Max, use CommonJS import
+const maxApi = require("max-api");          // for Max, use CommonJS import
+
+
+// Variables
+const serverURL = "wss://artistic-endurable-lifeboat.glitch.me";  // make sure you EDIT THIS!
+                                                               // use http://localhost:3000
+                                                               // if running server locally
+// Client Initialization
+const socket = io(serverURL);
+
+
+// RECEIVE
+
+socket.on("connect", () => {
+    maxApi.post("Connected to server!");
+ });
+
+socket.on("bang", () => {
+    maxApi.outletBang();
+});
+socket.on("message", (message) => {
+    maxApi.outlet("from-server", message);
+});
+
+
+// SEND
+
+maxApi.addHandler("bang", () => {
+	socket.emit("bang");
+});
+
+maxApi.addHandler("message", (message) => { // text from Max prepended with "message"
+	socket.emit("message", message);        // send "message" event to the server
+});
+
+maxApi.addHandler("audioData", (message) => { // data from Max prepended with "audioData
+	socket.emit("audioData", message);        // send "message" event to the server
+});
